14514a9272c393e0ca0534b066d516d7139ac02e
Jeff Smits <jeff.smits@gmail.com>
2016-07-12 12:02:19 +0100

    First experiment with outputting plots, not with the right interface yet

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#![feature(inclusive_range_syntax)]
 extern crate rustc_serialize;
 extern crate docopt;
 #[macro_use]
@@ -13,6 +14,7 @@ mod utils;
 use docopt::Docopt;
 use regex::Regex;
 use prettytable::format;
+use gnuplot::Figure;
 
 use benchmark::{Comparisons, Benchmarks, Benchmark, parse_benchmarks};
 
@@ -20,9 +22,7 @@ use std::io;
 use std::io::prelude::*;
 use std::fs::File;
 use std::collections::btree_map::BTreeMap;
-
-use OutputMode::*;
-use PlotSubject::*;
+use std::fmt;
 
 macro_rules! err_println {
     ($fmt:expr) => (err_print!(concat!($fmt, "\n")));
@@ -37,14 +37,17 @@ const USAGE: &'static str = r#"
 Compares Rust micro-benchmark results.
 
 Usage:
-    cargo benchcmp [options] <file> <file>
-    cargo benchcmp [options] <name> <name> <file>...
-    cargo benchcmp plot <file>...
+    cargo benchcmp file [options] <file> <file>
+    cargo benchcmp name [options] <name> <name> <file>...
+    cargo benchcmp plot [options] <bench> <file>...
     cargo benchcmp -h | --help
 
-The first version takes two file and compares the common bench-tests.
-The second version takes two module names and one or more files, and compares
-the common bench-tests of the two modules.
+Commands:
+    file                 takes two file and compares the common bench-tests.
+    name                 takes two module names and one or more files, and
+                         compares the common bench-tests of the two modules.
+    plot                 takes a bench-test name and one of more files, and
+                         plots a bar-chart of the bench-test in question.
 
 General options:
     -h, --help           show this help message and exit
@@ -57,7 +60,7 @@ Comparison options:
     --show <option>      show regressions, improvements or both [default: both]
     --strip-fst <regex>  a regex to strip from first benchmarks' names
     --strip-snd <regex>  a regex to strip from second benchmarks' names
-    --plot-cmp           plot the comparison instead of printing as table
+    --plot               plot the comparison instead of printing as table
                          (you can also set the --output-format when using this
                          option)
 
@@ -67,7 +70,7 @@ Plot command options (requires gnuplot):
                          name [default: module]
     --output-format <format>
                          Plot output formats are: gnuplot (the commands),
-                         pdf, eps, png [default: png]
+                         pdf, eps, png, svg [default: png]
 "#;
 
 #[derive(Debug, RustcDecodable)]
@@ -78,12 +81,13 @@ struct Args {
     flag_show: ShowOption,
     flag_strip_fst: Option<String>,
     flag_strip_snd: Option<String>,
-    flag_plot_cmp: bool,
+    flag_plot: bool,
     flag_plot_mode: ToolMode,
     flag_output_format: OutputFormat,
     cmd_plot: bool,
-    arg_name: Option<[String; 2]>,
     arg_file: Vec<String>,
+    arg_name: Option<[String; 2]>,
+    arg_bench: String,
 }
 
 #[derive(Debug, RustcDecodable, PartialEq, Eq)]
@@ -105,6 +109,20 @@ enum OutputFormat {
     Pdf,
     Eps,
     Png,
+    Svg,
+}
+
+impl fmt::Display for OutputFormat {
+    fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        use OutputFormat::*;
+        match *self {
+            Gnuplot => f.write_str("gnuplot"),
+            Pdf => f.write_str("pdf"),
+            Eps => f.write_str("eps"),
+            Png => f.write_str("png"),
+            Svg => f.write_str("svg"),
+        }
+    }
 }
 
 struct Settings {
@@ -132,14 +150,31 @@ struct ComparisonDetails {
 
 #[derive(Debug, PartialEq, Eq)]
 enum PlotSubject {
-    Everything,
+    Benchmark(String),
     Comparison(ComparisonDetails),
 }
 
 impl Args {
     fn into_settings(self) -> Settings {
+        use OutputMode::*;
+        use PlotSubject::*;
+
         Settings {
-            output_file: self.flag_output,
+            output_file: if self.cmd_plot && self.flag_output_format != OutputFormat::Gnuplot {
+                let name = Some(format!("benchcmp_{}.{}", self.arg_bench, self.flag_output_format));
+                if self.flag_output.is_none() {
+                    name
+                } else {
+                    let s = self.flag_output.unwrap();
+                    if s.is_empty() {
+                        name
+                    } else {
+                        Some(s)
+                    }
+                }
+            } else {
+                self.flag_output
+            },
             variance: self.flag_variance,
             filenames: self.arg_file.clone(),
             tool_mode: if self.cmd_plot {
@@ -151,7 +186,7 @@ impl Args {
                 }
             },
             out_mode: if self.cmd_plot {
-                Plot(Everything, self.flag_output_format)
+                Plot(Benchmark(self.arg_bench), self.flag_output_format)
             } else {
                 let details = ComparisonDetails {
                     names: self.arg_name,
@@ -160,7 +195,7 @@ impl Args {
                     strip_fst: self.flag_strip_fst,
                     strip_snd: self.flag_strip_snd,
                 };
-                if !self.flag_plot_cmp {
+                if !self.flag_plot {
                     Table(details)
                 } else {
                     Plot(Comparison(details), self.flag_output_format)
@@ -181,6 +216,9 @@ macro_rules! try_print_err {
 }
 
 fn main() {
+    use OutputMode::*;
+    use PlotSubject::*;
+
     let settings = {
         let args: Args = Docopt::new(USAGE)
             .and_then(|d| d.decode())
@@ -196,27 +234,35 @@ fn main() {
     match settings.out_mode {
         Plot(Comparison(ref details), _) |
         Table(ref details) => {
-            let pairs = try_print_err!(filter_benchmarks(benchmarks, details));
+            let benchmarks = try_print_err!(filter_benchmarks(benchmarks, details));
             // These benchmarks are maps "bench_name -> file/module -> benchmark"
-            let pairs = by_bench_name(pairs);
+            let benchmarks = by_bench_name(benchmarks);
 
             match settings.out_mode {
                 Plot(_, format) => {
-                    unimplemented!();
+                    //                    let figure = plot_benchmarks(benchmarks);
+                    //
+                    //                    write_plot(figure, format, settings.output_file);
                 }
                 Table(_) => {
-                    try_print_err!(write_pairs(settings.output_file,
-                                               pairs,
+                    try_print_err!(write_pairs(benchmarks,
+                                               settings.output_file,
                                                settings.variance,
                                                details));
                 }
             }
         }
-        Plot(Everything, format) => {
+        Plot(Benchmark(bench), format) => {
             // These benchmarks are maps "bench_name -> file/module -> benchmark"
             let benchmarks = by_bench_name(benchmarks);
 
-            unimplemented!();
+            let benchmark = benchmarks.into_iter()
+                .find(|c| c.bench_name == bench)
+                .expect(format!("ERROR: Cannot find benchmark {}", bench).as_str());
+
+            let figure = plot_benchmark(benchmark);
+
+            write_plot(figure, format, settings.output_file);
         }
     }
 }
@@ -309,8 +355,8 @@ fn filter_benchmarks(benchmarks: Vec<Benchmarks>,
 }
 
 /// Write the pairs of benchmarks in a table, along with their comparison
-fn write_pairs(file: Option<String>,
-               pairs: Vec<Comparisons>,
+fn write_pairs(pairs: Vec<Comparisons>,
+               file: Option<String>,
                variance: bool,
                details: &ComparisonDetails)
                -> Result<(), io::Error> {
@@ -354,6 +400,72 @@ fn write_pairs(file: Option<String>,
     Ok(())
 }
 
+fn plot_benchmark(comparisons: Comparisons) -> Figure {
+    use gnuplot::AxesCommon;
+    use gnuplot::Tick;
+    use gnuplot::TickOption;
+    use gnuplot::PlotOption;
+    use gnuplot::AutoOption;
+
+    /// Escapes strings for gnuplot. Since labels are wrapped in double quotes, we need *two*
+    ///  backslashes before every underscore to make it display the underscore.
+    fn escape(s: &String) -> String {
+        s.replace('_', r"\\_")
+    }
+
+    let mut figure = Figure::new();
+
+    {
+        let log_base = 10f64;
+        let xs = 0..comparisons.assocs.len();
+        let x_ticks: Vec<Tick<usize>> = xs.clone().map(|x| Tick::Major(x, AutoOption::Fix(escape(&comparisons.assocs[x].0)))).collect();
+        let ys: Vec<usize> = comparisons.assocs.iter().map(|t| t.1.ns).collect();
+        let y_err = comparisons.assocs.iter().map(|t| t.1.variance);
+        let y_min = comparisons.assocs.iter().map(|t| t.1.ns - t.1.variance).min().unwrap() as f64 * 0.98;
+        let y_max = comparisons.assocs.iter().map(|t| t.1.ns + t.1.variance).max().unwrap() as f64 * 1.02;
+        let bench_name = escape(&comparisons.bench_name);
+        let options = [PlotOption::Color("#555555"), PlotOption::BorderColor("#FFFFFF")];
+
+        figure.axes2d()
+            .set_title(bench_name.as_str(), &[])
+            .boxes(xs.clone(), ys.clone(), &options)
+            .set_x_ticks_custom(x_ticks, &[TickOption::Mirror(false)], &[])
+            .set_y_label("ns/iter (log 10 scale)", &[])
+            .set_y_log(Some(log_base))
+            .set_y_ticks(Some((AutoOption::Auto, 10)), &[], &[])
+            .set_y_range(AutoOption::Fix(y_min), AutoOption::Fix(y_max))
+            .y_error_lines(xs, ys, y_err, &[PlotOption::PointSize(0f64), PlotOption::LineWidth(2f64), PlotOption::Color("red")]);
+    }
+
+    figure
+}
+
+fn write_plot(mut figure: Figure, format: OutputFormat, file: Option<String>) {
+    use OutputFormat::*;
+
+    let filestr = file.as_ref().map(String::as_str).unwrap_or("");
+    let formatstr = match format {
+        Gnuplot => {
+            match file {
+                Some(ref s) => {
+                    figure.echo_to_file(s.as_str());
+                }
+                None => {
+                    figure.echo(&mut io::stdout());
+                }
+            }
+            return;
+        }
+        Pdf => "pdfcairo",
+        Eps => "epscairo",
+        Png => "pngcairo",
+        Svg => "svg",
+    };
+
+    figure.set_terminal(formatstr, filestr);
+    figure.show();
+}
+
 /// Filter the names in every benchmark, based on the regex string
 fn strip_names(mut benches: Benchmarks,
                strip: &Option<String>)
@@ -379,12 +491,12 @@ fn warn_missing(v: Vec<Comparisons>) {
     let mut map = BTreeMap::new();
 
     for comparisons in v {
-        map.entry(comparisons.assocs[0].0.clone()).or_insert_with(Vec::new).push(comparisons.bench_name);
+        map.entry(comparisons.assocs[0].0.clone())
+            .or_insert_with(Vec::new)
+            .push(comparisons.bench_name);
     }
 
-    for (k,v) in map {
-        err_println!("WARNING: found test(s) {:?} only in {}",
-                     v,
-                     k);
+    for (k, v) in map {
+        err_println!("WARNING: found test(s) {:?} only in {}", v, k);
     }
 }
